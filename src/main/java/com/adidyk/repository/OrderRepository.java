8fa97e167f76bef7d191466797d9660bb9854e3a
androsdav <androsdav@gmail.com>
2019-11-29 17:51:58 +0200

    bot

--- src/main/java/com/adidyk/repository/OrderRepository.java
+++ src/main/java/com/adidyk/repository/OrderRepository.java
@@ -3,12 +3,14 @@ package com.adidyk.repository;
 import com.adidyk.model.pojo.Order;
 import com.adidyk.model.pojo.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 /**
  * Interface OrderRepository.
  */
+@Repository
 public interface OrderRepository extends JpaRepository<Order, Integer> {
 
     List<Order> findAllByUser(User user);
--- src/main/java/com/adidyk/service/OrderService.java
+++ src/main/java/com/adidyk/service/OrderService.java
@@ -5,7 +5,6 @@ import com.adidyk.model.pojo.User;
 import com.adidyk.repository.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
 
 /**
