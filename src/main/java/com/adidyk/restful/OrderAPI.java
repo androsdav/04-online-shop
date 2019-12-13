287279595ecf5364ae323bba6e0fd918ad92829f
androsdav <androsdav@gmail.com>
2019-12-13 19:21:50 +0200

    bot

--- src/main/java/com/adidyk/restful/OrderAPI.java
+++ src/main/java/com/adidyk/restful/OrderAPI.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 /**
- * Class OrderAPI....,,,//
+ * Class OrderAPI....,,,//----
  */
 @RestController
 public class OrderAPI {
